724bdd55dce366b1f505124c1e3286c9aac0562d
David Sanchez <sanchez.dav90@gmail.com>
2023-09-26 22:50:35 -0600

    Test task item class

---
+++ test/task_item_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/task_item'
+
+class TaskItemTest < Minitest::Test
+  def test_valid_task_creation
+    task = TaskItem.new("Buy fruits", "Apples, Bananas, Oranges and Grapes")
+    assert_equal "Buy fruits", task.title
+    assert_equal "Apples, Bananas, Oranges and Grapes", task.description
+    refute task.completed
+  end
+
+  def test_invalid_task_creation
+    assert_raises ArgumentError do
+      TaskItem.new
+    end
+  end
+
+  def test_invalid_task_with_only_title
+    assert_raises ArgumentError do
+      TaskItem.new("Buy fruits")
+    end
+  end
+end
