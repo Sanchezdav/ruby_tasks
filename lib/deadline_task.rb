e648dd9dee40e44471afc22558f04a92c60575f0
David Sanchez <sanchez.dav90@gmail.com>
2023-09-26 23:54:11 -0600

    Create the deadline task

---
+++ lib/deadline_task.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'Date'
+require_relative './task_item'
+require_relative '../helpers/tasks'
+
+class DeadlineTask < TaskItem
+  attr_accessor :due_date
+
+  def initialize(title, description, due_date)
+    super(title, description)
+    @due_date = due_date
+  end
+
+  def parsed_due_date
+    @due_date.strftime("%d/%m/%Y")
+  end
+
+  def days_until_due
+    today = Date.today
+    days_until = (@due_date - today).to_i
+
+    if days_until < 0
+      "Due #{days_until} days ago"
+    elsif days_until == 0
+      "Due today"
+    else
+      "#{days_until} days left to due date"
+    end
+  end
+end
+
+puts "--- DeadlineTask ---"
+next_month = Date.today.next_month
+deadline = DeadlineTask.new("Pay bills", "Pay internet and car", next_month)
+puts deadline.print_attributes
+puts "Parsed date: #{deadline.parsed_due_date}"
+puts deadline.days_until_due
---
+++ test/deadline_task_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/deadline_task'
+
+class DeadlineTaskTest < Minitest::Test
+  def test_valid_task_creation
+    next_month = Date.today.next_month
+    deadline = DeadlineTask.new("Pay bills", "Pay internet and car", next_month)
+    assert_equal "Pay bills", deadline.title
+    assert_equal "Pay internet and car", deadline.description
+    assert_equal next_month, deadline.due_date
+    refute deadline.completed
+  end
+
+  def test_invalid_task_creation
+    assert_raises ArgumentError do
+      DeadlineTask.new
+    end
+  end
+
+  def test_invalid_task_with_only_title
+    assert_raises ArgumentError do
+      DeadlineTask.new("Pay bills")
+    end
+  end
+
+  def test_mark_as_completed
+    next_month = Date.today.next_month
+    deadline = DeadlineTask.new("Pay bills", "Pay internet and car", next_month)
+    refute deadline.completed
+    deadline.mark_as_completed
+    assert deadline.completed
+  end
+
+  def test_parsed_due_date
+    next_month = Date.today.next_month
+    deadline = DeadlineTask.new("Pay bills", "Pay internet and car", next_month)
+    assert_equal next_month.strftime("%d/%m/%Y"), deadline.parsed_due_date
+  end
+end
